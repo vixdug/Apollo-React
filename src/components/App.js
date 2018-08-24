106a3a7bcc70d16f8dfcac41b60c1913793387ba
Victoria Duggan <victoria.duggan@shopify.com>
2018-08-24 10:08:50 -0700

    adding search query and frontend

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import CreateLink from './CreateLink'
 import Header from './Header'
 import Login from './Login'
 import { Switch, Route } from 'react-router-dom'
+import Search from './Search'
+
 
 
 class App extends Component {
@@ -16,6 +18,7 @@ class App extends Component {
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/Login" component={Login} />
+          <Route exact path='/search' component={Search} />
         </Switch>
       </div>
     </div>
---
+++ src/components/Search.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { withApollo } from 'react-apollo'
+import gql from 'graphql-tag'
+import Link from './Link'
+
+
+const FEED_SEARCH_QUERY = gql`
+query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }`
+  
+class Search extends Component {
+
+  state = {
+    links: [],
+    filter: ''
+  }
+
+  render() {
+    return (
+      <div>
+         <div>
+           Search
+           <input
+             type='text'
+             onChange={e => this.setState({ filter: e.target.value })}
+           />
+           <button onClick={() => this._executeSearch()}>OK</button>
+         </div>
+         {this.state.links.map((link, index) => (
+           <Link key={link.id} link={link} index={index} />
+         ))}
+       </div>
+     )
+   }
+
+   _executeSearch = async () => {
+
+   }
+ }
+
+ export default withApollo(Search)
