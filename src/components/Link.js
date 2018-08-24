462206b7574a911a40bb27cdffda15374725a622
Victoria Duggan <victoria.duggan@shopify.com>
2018-08-24 09:28:46 -0700

    reworking cache for vote update

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,4 +1,26 @@
 import React, { Component } from 'react'
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const VOTE_MUTATION = gql`
+mutation VoteMutation($linkId: ID!) {
+  vote(linkId: $linkId) {
+    id
+    link {
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+    user {
+      id
+    }
+  }
+}`
 
 class Link extends Component {
   render() {
@@ -6,12 +28,19 @@ class Link extends Component {
     return (
       <div className="flex mt2 items-start">
         <div className="flex items-center">
-          <span className="gray">{this.props.index + 1}.</span>
-          {authToken && (
-            <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
-              ▲
-            </div>
-          )}
+        <span className="gray">{this.props.index + 1}.</span>
+         {authToken && (
+           <Mutation mutation={VOTE_MUTATION} variables={{ linkId: this.props.link.id }}
+           update={(store, {data: { vote }}) =>
+         this.props.updateStoreAfterVote(store, vote, this.props.link.id)}
+         >
+             {voteMutation => (
+               <div className="ml1 gray f11" onClick={voteMutation}>
+                 ▲
+               </div>
+             )}
+           </Mutation>
+         )}
         </div>
         <div className="ml1">
           <div>
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -11,11 +11,31 @@ const FEED_QUERY = gql`
     createdAt
     url
     description
+    postedBy {
+      id
+      name
+    }
+    votes {
+      id
+      user {
+        id
+        }
+      }
     }
   }
 }`
 
 class LinkList extends Component {
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -27,7 +47,14 @@ class LinkList extends Component {
 
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+                <Link
+                key={link.id}
+                link={link}
+                index={index}
+                updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
             </div>
           )
         }}
